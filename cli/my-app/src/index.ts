7c2b28b810f860e00fa08ad7c13119a0eea9a6f4
Hrutav Modha <modhahrutav@gmail.com>
2025-08-18 20:27:16 +0530

    Fixed few configuration bugs in JavaScript templates

--- cli/my-app/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { render, setRender } from 'blazzy-dom'
-import App from './App'
-const root = document.getElementById('root') as HTMLDivElement
-render(App(), root)
-setRender(() => render(App(), root))
